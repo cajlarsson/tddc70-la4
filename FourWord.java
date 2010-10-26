467493b50cec0ee2f3dc466b934a4b3aa78420a9
Caj Larsson <caj.larsson@gmail.com>
2010-10-26 10:41:58 +0200

    apapa

--- FourWord.java
+++ FourWord.java
@@ -1,70 +1,36 @@
-
-class FourWord
+abstract class FourWord
 {
-   public static void main (String args[])
+   public static  int toInt(String in)
    {
-      FourWord hej = new FourWord("abcd");
-      System.out.print(hej.toString());
-   }
-
-   final char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-			     'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-			     'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö', 'é' };
-
-   final int lookup( char c)
-   {
-      for (int i = 0; i < 30; i++)
-      {
-	 if ( c== alphabet[i])
-	    return i;
-      }
-      return -1;
-   }
-
-   private int value;
-
-   
-   public FourWord(String in)
-   {
-      
+      int value = 0;
       if (in.length() == 4)
       {
-	 char[] temp = in.toCharArray();
-
-	 for(int i= 1; i < 4 ; i++)
+	 byte[] temp = in.getBytes();
+	 
+	 for(int i= 0; i < 4 ; i++)
 	 {
-	    value |= ((int) lookup((temp[i])) << (i *5));
-	    	    
+	    value |= temp[i] << i;
 	 }
+	 return value;
       }
+      return 0xFFFFFFFF;
    }
    
-   public boolean neighbour(FourWord comp)
+   public static boolean neighbour(int v1, int v2)
    {
-      return ((comp.value & 0xFFFFFF00) == (value & 0xFFFFFF00))
-	 || ((comp.value & 0xFFFF00FF) == (value & 0xFFFF00FF))
-	 || ((comp.value & 0xFF00FFFF) == (value & 0xFF00FFFF))
-	 || ((comp.value & 0x00FFFFFF) == (value & 0x00FFFFFF));
+       return ((v1 & 0x1FFFE0 ) == (v2 & 0x1FFFE0))
+	 || ((v1 & 0x1FFC1F) == (v2 & 0x1FFC1F ))
+	 || ((v1 & 0x1F83FF) == (v2 & 0x1F83FF))
+	 || ((v1 & 0xFFFF) == (v2 & 0xFFFF));
    }
 
-   public boolean equal(FourWord comp)
-   {
-      return comp.value == value;
-   }
- 
-   public String toString()
+   public static String toString(int value)
    {
       char[] tmp = new char[4];
       for( int i = 0; i < 4; i++)
       {
 	 tmp[i] = alphabet[(value >> (i * 5)& 0x1F)] ;
       }
-
       return new String(tmp);
-   }
-
-   public int toInt()
-   {
-      return value;
    }
 }
